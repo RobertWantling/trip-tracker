b8b22d7dcdd6d296341d260e512a97f500ff1eeb
RobertWantling <robert6995@hotmail.co.uk>
2023-07-13 14:23:42 +0100

    create initial pages and input fields

---
+++ app.js
@@ -0,0 +1,20 @@
+'use script';
+
+// ignore for prettier
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+// all handlers
+const form = document.querySelector('.form');
